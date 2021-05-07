da56b0d29fb47d9085ec6cfa5af00bdaaafa5bc3
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2021-05-07 12:49:29 +0530

    Style:made changes in style of carousal main page

--- src/Components/MainPage/Carousal.js
+++ src/Components/MainPage/Carousal.js
@@ -2,17 +2,19 @@ import { Carousel } from "react-responsive-carousel";
 import { imagesArray } from "./images";
 export const CarousalComponent = () => {
   return (
-    <Carousel
-      showThumbs={false}
-      showStatus={false}
-      autoPlay={true}
-      infiniteLoop={true}
-    >
-      {imagesArray.map((item, index) => (
-        <div key={index}>
-          <img alt="caurosal" style={{ height: "500px" }} src={item} />
-        </div>
-      ))}
-    </Carousel>
+    <div className="carousal-container">
+      <Carousel
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        infiniteLoop={true}
+      >
+        {imagesArray.map((item, index) => (
+          <div key={index}>
+            <img alt="caurosal" style={{ maxHeight: "500px" }} src={item} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
   );
 };
--- src/Components/MainPage/Main.js
+++ src/Components/MainPage/Main.js
@@ -3,9 +3,10 @@ import { Categories } from "./Category";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 export const Main = () => {
   return (
-    <>
+    <div className="main">
       <CarousalComponent />
+      <h2>Categories</h2>
       <Categories />
-    </>
+    </div>
   );
 };

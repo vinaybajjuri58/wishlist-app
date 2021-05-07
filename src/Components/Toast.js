b7f1028119c497bc67e3f022a357f699634a44e7
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2021-05-07 15:59:29 +0530

    Style:changed style of toast

--- src/Components/Toast.js
+++ src/Components/Toast.js
@@ -11,25 +11,8 @@ export const Toast = () => {
     };
   }, [toast, setToast]);
   return (
-    <div
-      style={{
-        position: "fixed",
-        padding: "1rem",
-        top: "2rem",
-        right: "0.25rem",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-around",
-        backgroundColor: "grey",
-        margin: "20px",
-        // border: "10px solid white",
-        width: "250px",
-        height: "40px",
-        color: "white",
-      }}
-    >
+    <div className="snackbar">
       <p>{toastMessage}</p>
-      <button onClick={() => setToast(false)}>X</button>
     </div>
   );
 };
